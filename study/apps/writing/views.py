baef71c73671ae5fae031a8961c511c016760fa2
TENCHIUDN <hrs2421506@stu.o-hara.ac.jp>
2025-11-21 14:14:59 +0900

    writing_top

---
+++ study/apps/writing/views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, redirect, render_template
+
+# アプリの作成
+writing = Blueprint(
+  'writing',
+  __name__,
+  # 使用するフォルダ
+  template_folder='templates',
+  static_folder='static'
+)
+
+@writing.route('/')
+def index():
+  # templates/crud/index.htmlとなる
+  return render_template('wiring/index.html')
+
+@writing.route('/')
+def writing_top():
+  # templates/crud/index.htmlとなる
+  return render_template('wiring/writing_top.html')
